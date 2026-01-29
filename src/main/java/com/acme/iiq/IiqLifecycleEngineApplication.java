5ab89025bdf072b1afd0dbbafc93c9c0cc1817c9
Omar Benaissa <omar.benaissa@outlook.com>
2026-01-29 01:44:18 +0100

    chore(config): configure postgres datasource + flyway + jpa validation

---
+++ src/main/java/com/acme/iiq/IiqLifecycleEngineApplication.java
@@ -0,0 +1,13 @@
+package com.acme.iiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IiqLifecycleEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IiqLifecycleEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/iiq/lifecycleengine/IiqLifecycleEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.iiq.lifecycleengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IiqLifecycleEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
